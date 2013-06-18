b2d6f817072f63e7c323fc1530e3213ecdd82280
Doug Morgan <douglas.morgan3405@gmail.com>
2013-06-17 21:11:47 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JsFundies::Application.config.secret_token = 'e755c50e54b6f4bdfbea14ff7defda70cfc7f1b75d6300a88dcce6872d9a498df453dc439e058e270094e727ee25991167636828b9ec11afcb1e663cbc124f6c'
